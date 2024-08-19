228fdf2ea3329d8ca4753f52c11efb0dd26e54be
Akhil Deepak <akhildeepak01@gmail.com>
2024-08-19 12:50:34 +0000

    adding new task

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let toDoItems = [];
+let todoItem = 0;
 
 function addTodo() {
     const toDoText = document.querySelector("#toDoField").value;
     toDoItems.push(toDoText);
+    const toDoList = document.querySelector("#todoList");
+    toDoList.innerHTML+= `<li>${todoList[todoItem]}<button class="delete-button" id="delete-${todoItem}" onclick="deleteTodo(this.id)">
+    <i class="fa fa-trash" aria-hidden="true"></i></button>
+    <button class="edit-botton" id="edit-${todoItem}" onclick="editTodo(this.id)">
+    <i class="fa fa-pencil" aria-hidden="true"></i></button>
+    <button class="done-botton" id="done-${todoItem}" onclick="doneTodo(this.id)">
+    <i class="fa fa-check" aria-hidden="true"></i>
+    </button>
+    </li>`;
+    todoItem += 1;
+
 }
