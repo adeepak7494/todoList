394ab8ae6dbdf19aff1fd3c8c6f8f2fe0164a358
Akhil Deepak <akhildeepak01@gmail.com>
2024-08-20 00:37:14 +0000

    small change

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function addTodo() {
     toDoItems.push(toDoText);
     console.log(toDoItems[todoItem])
     let toDoList = document.querySelector("#todoList");
-    toDoList.innerHTML+= `<li>${toDoItems[todoItem]}<button class="delete-button" id="delete-${todoItem}" onclick="deleteTodo(this.id)">
+    toDoList.innerHTML += `<li>${toDoItems[todoItem]}<button class="delete-button" id="delete-${todoItem}" onclick="deleteTodo(this.id)">
     <i class="fa fa-trash" aria-hidden="true"></i></button>
     <button class="edit-botton" id="edit-${todoItem}" onclick="editTodo(this.id)">
     <i class="fa fa-pencil" aria-hidden="true"></i></button>
