e53735a0fc0e63e194f1f9f2a7cacda4ca16e70c
adeepak7494 <akhildeepak01@gmail.com>
2024-08-25 02:09:20 +0530

    initial material UI look and feel completed

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ let toDoItemsList = [];
 let todoItem = 0;
 
 function addTodo() {
-  let toDoText = document.getElementById("toDoField").value;
-  toDoItemsList.push(toDoText);
-  console.log(toDoItemsList[todoItem]);
-  document.getElementById(
-    "todoUl"
-  ).innerHTML = `<li>${toDoItemsList[todoItem]}<button class="delete-button" id="delete-${todoItem}" onclick="deleteTodo(this.id)">
-    <i class="fa fa-trash" aria-hidden="true"></i></button>
-    <button class="edit-button" id="edit-${todoItem}" onclick="editTodo(this.id)">
-    <i class="fa fa-pencil" aria-hidden="true"></i></button>
-    <button class="done-button" id="done-${todoItem}" onclick="doneTodo(this.id)">
-    <i class="fa fa-check" aria-hidden="true"></i>
-    </button>
-    </li>`;
-  document.getElementById("toDoField").value = "";
-  todoItem += 1;
+  let toDoText = document.querySelector("#toDoField");
+  if(toDoText.value == "") {
+    toDoText.target.checkValidity();
+  } else {
+    toDoItemsList.push(toDoText.value);
+    console.log(toDoItemsList[todoItem]);
+    document.getElementById(
+      "todoList"
+    ).innerHTML += `<li id="toDoItem"><input type="checkbox" id="expand-${todoItem}-button">${toDoItemsList[todoItem]}<button class="expand-task-button" id="expand-${todoItem}-button" onclick="ex[andTask(this.id)">
+      <i class="fa-solid fa-chevron-right"></i></button>
+      </li>
+      <hr class="taskDivider">`;
+    toDoText.value = "";
+    todoItem += 1;
+  }
+  
 }
 
