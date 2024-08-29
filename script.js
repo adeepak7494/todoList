7baaafbba53420f49428717689aa3a6935b788a2
Akhil Deepak <akhildeepak01@gmail.com>
2024-08-29 17:51:48 +0530

    stuck in transition of expanding task

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ function addTodo() {
   if(toDoText.value == "") {
     toDoText.target.checkValidity();
   } else {
-    toDoItemsList.push(toDoText.value);
-    console.log(toDoItemsList[todoItem]);
+    toDoItemsList.push({title : toDoText.value, description : "", listType : "", tags: "" });
     document.getElementById(
       "todoList"
-    ).innerHTML += `<li id="toDoItem"><input type="checkbox" id="expand-${todoItem}-button">${toDoItemsList[todoItem]}<button class="expand-task-button" id="expand-${todoItem}-button" onclick="ex[andTask(this.id)">
+    ).innerHTML += `<li id="toDoItem"><input type="checkbox" id="expand-${todoItem}-button">${toDoItemsList[todoItem].title}<button class="expand-task-button" id="expand-${todoItem}-button" onclick="expandTask(this.id)">
       <i class="fa-solid fa-chevron-right"></i></button>
       </li>
       <hr class="taskDivider">`;
@@ -18,5 +17,38 @@ function addTodo() {
     todoItem += 1;
   }
   
+}
+
+function openSideNav() {
+  let sideBar = document.querySelector('#sidebar');
+  if(sideBar.style.width == "20%" || sideBar.style.width == "") {
+    sideBar.style.width = "5%";
+    document.getElementById('appTitle').style.display = "none";
+    let Nodelist = sideBar.children;
+    for(let i =1; i< Nodelist.length; i++) {
+        for(let j = 0; j< Nodelist[i]?.children.length; j++) {
+          if(Nodelist[i]) {
+            Nodelist[i].style.display = "none";          
+          }
+        }     
+    }
+    
+  } else {
+    sideBar.style.width = "20%"
+    document.getElementById('appTitle').style.display = "";
+    let Nodelist = sideBar.children;
+    for(let i =1; i< Nodelist.length; i++) {
+      for(let j = 0; j< Nodelist[i]?.children.length; j++) {
+        if(Nodelist[i]) {
+          Nodelist[i].style.display = "";          
+        }
+      }     
+    }
+  }
+}
+
+function expandTask(clickedId) {
+console.log(clickedId)
+document.querySelector("#taskDetailBlock").classlist.toggle('is-show');
 }
 
